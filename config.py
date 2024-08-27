184e41b0e99f6b2ee6d9435dd775b7b89e85da61
pawan <pawan@example.com>
2024-08-27 17:19:42 +0530

    init commit

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+import requests
 class config:
-   api_key = 'XXXXX'
-   username = 'XXXXX'
-   pwd = 'XXXX'
+   api_key = 'XXXX'
+   username = 'XXXX'
+   pwd = 'XXXXX'
    token = "XXXXXX"
-   azure_storage_connection_string ="XXXX"
+   azure_storage_connection_string ="XXXXXXX"
    time_zone ="Asia/Kolkata"
+   setting_url ="XXXXX"
+
+   @staticmethod
+   def get_setting_from_url():          
+       response_data =  requests.get(config.setting_url) 
+       return response_data.text 
